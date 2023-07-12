9fafab30b75cab1be36539e86f625a13c9f5cf73
theonnogueira <theonnogueira@outlook.com>
2023-07-12 16:01:41 -0300

    anotacoes JPA para classes de dominio

---
+++ src/main/java/com/theo/helpdesk/domain/Tecnico.java
@@ -0,0 +1,36 @@
+package com.theo.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.theo.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+}
