39975662a461d7d9217588789b4dd179339060f4
theonnogueira <theonnogueira@outlook.com>
2023-08-16 09:35:56 -0300

    Implementando tratamento de exceção

--- src/main/java/com/theo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/theo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.theo.helpdesk.domain.Tecnico;
 import com.theo.helpdesk.repositories.TecnicoRepository;
+import com.theo.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
