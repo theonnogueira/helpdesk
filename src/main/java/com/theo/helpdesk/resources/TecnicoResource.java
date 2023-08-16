074790763ba692cf39673b1d34fe0516d627ba8d
theonnogueira <theonnogueira@outlook.com>
2023-08-16 08:33:12 -0300

    Implementando controlador e service para tecnico e adicionando anotação JsonIgnore na model

---
+++ src/main/java/com/theo/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,27 @@
+package com.theo.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.theo.helpdesk.domain.Tecnico;
+import com.theo.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+
+	@Autowired
+	private TecnicoService tecService;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico obj = this.tecService.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
