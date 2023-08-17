8e880bf1be04143e9c9a61eef30f7216e8454781
theonnogueira <theonnogueira@outlook.com>
2023-08-17 07:35:43 -0300

    Validação de campos de criação do novo usuário

--- src/main/java/com/theo/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/theo/helpdesk/domain/dtos/TecnicoDTO.java
@@ -1,10 +1,13 @@
 package com.theo.helpdesk.domain.dtos;
 
 import java.io.Serializable;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.theo.helpdesk.domain.Tecnico;
@@ -14,9 +17,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "O campo NOME é requerido.")
 	protected String nome;
+	@NotNull(message = "O campo CPF é requerido.")
 	protected String cpf;
+	@NotNull(message = "O campo E-MAIL é requerido.")
 	protected String email;
+	@NotNull(message = "O campo SENHA é requerido.")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 
