7777ff2c933ab1406ca8abff2480bdb35bb53f71
theonnogueira <theonnogueira@outlook.com>
2023-08-23 14:20:52 -0300

    Método create para chamado

--- src/main/java/com/theo/helpdesk/services/ChamadoService.java
+++ src/main/java/com/theo/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.theo.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.theo.helpdesk.domain.Chamado;
+import com.theo.helpdesk.domain.Cliente;
+import com.theo.helpdesk.domain.Tecnico;
+import com.theo.helpdesk.domain.dtos.ChamadoDTO;
+import com.theo.helpdesk.domain.enums.Prioridade;
+import com.theo.helpdesk.domain.enums.Status;
 import com.theo.helpdesk.repositories.ChamadoRepository;
 import com.theo.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
@@ -23,5 +34,26 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return chamadoRepository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return chamadoRepository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+
+		Chamado chamado = new Chamado();
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
